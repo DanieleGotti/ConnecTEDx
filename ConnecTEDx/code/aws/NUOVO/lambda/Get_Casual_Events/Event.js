b0e0f25828b900fe0591f078ddcf1d71e8b6f72a
DanieleGotti <danielegotti02@gmail.com>
2024-07-09 18:59:49 +0200

    Aws scripts

---
+++ ConnecTEDx/code/aws/NUOVO/lambda/Get_Casual_Events/Event.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const event_schema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    event_url: String,
+    image_url: String,
+    start_date: String,
+    city: String,
+    price: String
+}, { collection: 'event_data' });
+
+module.exports = mongoose.model('event', event_schema);
---
+++ ConnecTEDx/code/aws/NUOVO/lambda/Get_Casual_Events/handler.js
@@ -0,0 +1,50 @@
+const connect_to_db = require('./db');
+const Event = require('./Event'); // Import the correct Event model
+
+module.exports.get_casual_events = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    let body = {}
+    if (event.body) {
+        body = JSON.parse(event.body);
+    }
+
+    if (!body.doc_per_page) {
+        body.doc_per_page = 10;
+    }
+    if (!body.page) {
+        body.page = 1;
+    }
+
+    connect_to_db().then(() => {
+        console.log('=> get_all events');
+
+        Event.aggregate([
+            { $sample: { size: body.doc_per_page } }, 
+            {
+                $project: {
+                    name: 1,
+                    event_url: 1,
+                    image_url: 1,
+                    start_date: 1,
+                    city: 1,
+                    price: 1
+                }
+            } 
+        ])
+        .then(events => {
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(events)
+                });
+            }
+        )
+        .catch(err =>
+            callback(null, {
+                statusCode: err.statusCode || 500,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Could not fetch the events.'
+            })
+        );
+    });
+};
